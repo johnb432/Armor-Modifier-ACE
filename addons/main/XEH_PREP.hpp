820cd85775c06efa18fc9da2e1de4a98f41d175f
johnb432 <58661205+johnb432@users.noreply.github.com>
2024-12-08 09:25:21 +0100

    1.0.4.0

--- addons/main/XEH_PREP.hpp
+++ addons/main/XEH_PREP.hpp
@@ -1,5 +1,1 @@
-if (getNumber (configFile >> "CfgPatches" >> "ace_main" >> "version") >= 3.18) then {
-    PREP(handleDamage,handleDamage);
-} else {
-    PREP(handleDamage,handleDamageOld);
-};
+PREP(handleDamage);
